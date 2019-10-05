3da8688a587d3053bf51c5e530ae53ec98207fab
Shalva Gegia <shalva.gegia@gmail.com>
2019-10-05 20:54:17 +0200

    chore: link

--- packages/api/routes/index.js
+++ packages/api/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var common = require('@workspace/common');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', common: common });
 });
 
 module.exports = router;
--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import common from '@workspace/common';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <pre>
+          {common.SHARE_VAR}
+        </pre>
         <a
           className="App-link"
           href="https://reactjs.org"
