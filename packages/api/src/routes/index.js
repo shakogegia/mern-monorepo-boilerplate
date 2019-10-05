36189246f9bd0124d64a4e9ea159ea922cb50285
Shalva Gegia <shalva.gegia@gmail.com>
2019-10-06 01:08:00 +0200

    chore: lint

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -1,10 +1,11 @@
-var express = require("express")
-var router = express.Router()
-var common = require("@workspace/constants")
+const express = require("express")
+
+const router = express.Router()
+const common = require("@workspace/constants")
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express", common: common })
+router.get("/", function(req, res) {
+  res.render("index", { title: "Express", common })
 })
 
 module.exports = router
