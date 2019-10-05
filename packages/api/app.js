056501af33026886021d819c3754f4e5518fb8cb
Shalva Gegia <shalva.gegia@gmail.com>
2019-10-05 23:14:23 +0200

    chore: precommit

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,5 +1,5 @@
-const createError = require('http-errors')
-const express = require('express')
+const createError = require("http-errors")
+const express = require("express")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
