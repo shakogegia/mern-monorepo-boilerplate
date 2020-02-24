d1d85be0a60e8c2dcd6b1f03230bb302eb4f7f91
shakogegia <shalva.gegia@gmail.com>
2020-02-24 15:13:30 +0100

    test

---
+++ tools/setup-env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Env Type SE | PROD
+ENV_TYPE=$1
+
+# Environment Variables list
+ENV_VARS="${*:2}"
+
+# Set env vars
+for var in $(echo $ENV_VARS)
+ do 
+  ENV_VAR_NAME="${var}_${ENV_TYPE}"
+  
+  eval "value=\${$ENV_VAR_NAME}"
+  
+  declare $var="$value"
+  export $var="$value"
+done
+
+
+[[ $DB == 1 ]] && ENV_TYPE="YES" || ENV_TYPE="NO"
+
+echo $ENV_TYPE
