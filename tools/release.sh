6ed2da41ca7fa5ddd290b45034686239219776ea
shakogegia <shalva.gegia@gmail.com>
2020-02-24 17:38:36 +0100

    test: asd

--- tools/release.sh
+++ tools/release.sh
@@ -23,6 +23,6 @@ DIFF=$(yarn lerna diff ${PACKAGE_NAME})
 if (( $(grep -c . <<<"$DIFF") > 3 )); then
   yarn lerna version --create-release github --ignore-changes '**' --conventional-commits --conventional-graduate --force-publish ${PACKAGE_NAME}
 else
-  yarn lerna version --create-release github --ignore-changes '**' --conventional-commits --conventional-graduate --force-publish ${PACKAGE_NAME}
+  yarn lerna version --create-release github --ignore-changes '**' --conventional-commits --force-publish ${PACKAGE_NAME}
   echo "✅  ${PACKAGE_NAME} has no changes since last release"
 fi 
